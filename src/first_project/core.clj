3cb44aeeed13617cb08922b56b1c2e0fcedb267b
kleberaluizio <kleberaluizio@gmail.com>
2025-02-07 23:29:17 -0300

    First function and first comment

--- src/first_project/core.clj
+++ src/first_project/core.clj
@@ -1,8 +1,14 @@
 (ns first-project.core)
 
+; Syntax is simple, it uses prefix notation, aka polish notation
+(defn plusTen [u] (+ u 10))
+
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
 
 (foo "Kleber")
+
+; First Comment
+(plusTen 1)
